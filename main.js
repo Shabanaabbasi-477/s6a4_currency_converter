8b135b18ce7322cc22d649a3dfc10178da1a1f9d
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-04-08 11:11:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgMagentaBright.bold("\n \t WELL COME TO CURRENCY CONVERSION APP \n"));
+// define the list of currencies and thier exchange rates.
+let exchange_rate = {
+    "USD": 1,
+    "EUR": 0.9,
+    "JYP": 113,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 270, //Pakistani Rupees  (PKR)
+    // We can select more currencies and their exchange rates in this object.
+};
+//Prompt the user to select currencies to convert from and to
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: chalk.blueBright.bold("Select the currency to convert from :"),
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: chalk.magentaBright.bold("Select the currency to convert into :"),
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: chalk.blueBright.bold("Enter the amount to convert :")
+    }
+]);
+//perform currency coversion by using a mathematcal formula
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount;
+//formulas using for currency conversion
+let base_amount = amount / from_amount;
+let converted_amount = base_amount * to_amount;
+console.log(chalk.yellowBright.bold(`Converted Amount = ${converted_amount.toFixed()}`));
